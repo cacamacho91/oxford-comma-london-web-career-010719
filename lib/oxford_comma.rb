6163727fa32221234d6e247da94eba3695cab8f1
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-25 19:53:52 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,7 @@ def oxford_comma(array)
   when 2
     return "#{array[0]} and #{array[1]}"
   else
+    string_list = array[0...-1].join(",")
     array.join(",")
 
     #return "#{array[0]}, #{array[1]} and #{array[2]}"
