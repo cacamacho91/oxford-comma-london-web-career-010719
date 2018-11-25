7fb63da93fea2e0ef37514749b71811c30053061
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-25 19:48:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,9 @@
 def oxford_comma(array)
   return_string = ""
   array.each_with_index do |item, index|
-    return_string << "#{item}, "
-    
+    if index+1 == array.size 
+      return_string << " and #{item}"
+    else
+      return_string << "#{item}, "
+    end
 end
