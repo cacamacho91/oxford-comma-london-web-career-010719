c3ce5a7b2987e3016971ad0d875ee220d777ad8e
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-25 19:50:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     return array[0]
   when 2
     return "#{array[0]} and #{array[1]}"
-  when > 3
+  when >3
 
     #return "#{array[0]}, #{array[1]} and #{array[2]}"
   end
