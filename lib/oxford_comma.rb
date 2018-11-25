b5f503f8421356ba0c03589b8388b9e0a2747379
dashing-frame-3501 <cacamacho91@gmail.com>
2018-11-25 19:44:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
   when 2
     return "#{array[0]} and #{array[1]}"
   when 3
-    return "#{array[0]}, #{array[1]} and #{array[2]}"
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
   end
 end
